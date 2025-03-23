ec6d614497d13aef1405ab448f0fb94023436fc8
Willy <willy_1020@naver.com>
2025-03-23 11:43:09 +0900

    [🔬 setting] 기본 Springboot 세팅

---
+++ src/main/java/org/terning/TerningApplication.java
@@ -0,0 +1,13 @@
+package org.terning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerningApplication.class, args);
+	}
+
+}
