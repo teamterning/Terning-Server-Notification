e5a5972b6387ac18cd03598b3f39d698f327e1a3
권장순 <jsoonworld@gmail.com>
2025-03-26 13:43:02 +0900

    feat(UserErrorCode) : 푸시 알림 관련 에러 코드 구현
    - 푸시 토큰 null 허용 불가 에러 코드 추가
    - 푸시 상태가 ON, OFF 외의 값일 경우의 에러 코드 추가
    - 푸시 알림이 이미 활성화/비활성화된 상태에 대한 예외 코드 추가

--- src/main/java/org/terning/user/common/failure/UserErrorCode.java
+++ src/main/java/org/terning/user/common/failure/UserErrorCode.java
@@ -13,6 +13,11 @@ public enum UserErrorCode implements ErrorCode {
     INVALID_USER_NAME(HttpStatus.BAD_REQUEST, "이름의 구성은 문자(한글, 영어), 숫자만 가능합니다."),
 
     PUSH_TOKEN_NOT_NULL(HttpStatus.BAD_REQUEST, "푸시 토큰은 null일 수 없습니다."),
+
+    INVALID_PUSH_STATUS(HttpStatus.BAD_REQUEST, "푸시 상태는 ON, OFF만 가능합니다."),
+
+    ALREADY_ENABLED_PUSH_NOTIFICATION(HttpStatus.BAD_REQUEST, "이미 푸시 알림이 활성화되어 있습니다."),
+    ALREADY_DISABLED_PUSH_NOTIFICATION(HttpStatus.BAD_REQUEST, "이미 푸시 알림이 비활성화되어 있습니다."),
     ;
 
     private static final String PREFIX = "[USER ERROR] ";
