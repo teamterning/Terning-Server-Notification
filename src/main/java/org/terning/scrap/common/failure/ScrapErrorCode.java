bc72e26a6cd09f1a783c9ad366fb4fb06a769f72
권장순 <jsoonworld@gmail.com>
2025-03-27 16:04:37 +0900

    feat(ScrapErrorCode) : 스크랩 상태 null 검증용 예외 코드 추가
    - ScrapStatus 입력값이 null인 경우를 명확히 처리하기 위해 SCRAP_STATUS_CANNOT_BE_NULL 코드 추가
    - from() 메서드 내 null 체크와 연계하여 예외 메시지 명확화

--- src/main/java/org/terning/scrap/common/failure/ScrapErrorCode.java
+++ src/main/java/org/terning/scrap/common/failure/ScrapErrorCode.java
@@ -8,9 +8,11 @@ import org.terning.global.failure.ErrorCode;
 @Getter
 @AllArgsConstructor
 public enum ScrapErrorCode implements ErrorCode {
+
     INVALID_SCRAP_STATUS(HttpStatus.BAD_REQUEST, "스크랩 상태는 scrapped, unscrapped만 가능합니다."),
     ALREADY_SCRAPPED(HttpStatus.BAD_REQUEST, "스크랩된 상태입니다."),
     ALREADY_UNSCRAPPED(HttpStatus.BAD_REQUEST, "스크랩되지 않은 상태입니다."),
+    SCRAP_STATUS_CANNOT_BE_NULL(HttpStatus.BAD_REQUEST, "스크랩 상태는 null일 수 없습니다."),
 
     ;
 
