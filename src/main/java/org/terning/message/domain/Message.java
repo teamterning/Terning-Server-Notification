381f6993938718852577e208ac9e07e62f4ce5fe
권장순 <jsoonworld@gmail.com>
2025-03-27 23:26:33 +0900

    refactor(Message) : 메시지 도메인 클래스 이름 변경 및 정리
    - Messages → Message로 클래스명 단수형으로 변경하여 의미 명확화
    - 동일한 필드(mainMessage, subMessage)는 유지
    - 기존 Messages 클래스 제거 및 새로운 Message 클래스 적용

--- src/main/java/org/terning/message/domain/Message.java
+++ src/main/java/org/terning/message/domain/Message.java
@@ -16,7 +16,7 @@ import org.terning.global.entity.BaseEntity;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Messages extends BaseEntity {
+public class Message extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
