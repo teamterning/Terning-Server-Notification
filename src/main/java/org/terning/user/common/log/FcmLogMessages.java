874c583c24f9c7021fa6729c928e46c5151dff64
Willy <willy_1020@naver.com>
2025-03-28 13:29:23 +0900

    [✅ chore] 로그 메세지를 별도의 핸들러 클래스로 관리하도록 수정

--- src/main/java/org/terning/user/common/log/FcmLogMessages.java
+++ src/main/java/org/terning/user/common/log/FcmLogMessages.java
@@ -5,8 +5,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public final class FcmLogMessages {
 
+    // 성공
     public static final String INIT_SUCCESS = "✅Firebase 초기화 성공";
-    public static final String INIT_FAILED = "❌Firebase 초기화 실패";
     public static final String PUSH_SENT_SUCCESS = "📤푸시 알림 전송 성공";
+
+    // 실패
+    public static final String INIT_FAILED = "❌Firebase 초기화 실패";
     public static final String PUSH_SENT_FAILURE = "❗푸시 알림 전송 실패";
 }
