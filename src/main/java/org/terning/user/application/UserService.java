e477b2ed0bfc906d0d737cdafc4bebfee53548ef
Willy <willy_1020@naver.com>
2025-03-29 22:16:38 +0900

    [✨ feat] 운영서버로부터 회원가입시 통신하는 알림서버 API 구현

--- src/main/java/org/terning/user/application/UserService.java
+++ src/main/java/org/terning/user/application/UserService.java
@@ -2,17 +2,41 @@ package org.terning.user.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.terning.user.application.fcmtoken.validate.FcmTokenValidateService;
+import org.terning.user.domain.User;
+import org.terning.user.domain.UserRepository;
+import org.terning.user.domain.vo.AccountStatus;
+import org.terning.user.domain.vo.FcmToken;
+import org.terning.user.domain.vo.PushNotificationStatus;
+import org.terning.user.domain.vo.UserName;
+import org.terning.user.dto.request.CreateUserRequest;
 import org.terning.user.dto.request.FcmTokenReissueRequiredRequest;
 import org.terning.user.dto.response.FcmTokenReissueRequiredResponse;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
 
     private final FcmTokenValidateService fcmTokenValidateService;
+    public final UserRepository userRepository;
 
     public FcmTokenReissueRequiredResponse isFcmTokenReissueRequired(FcmTokenReissueRequiredRequest request) {
         return fcmTokenValidateService.isFcmTokenReissueRequired(request);
+    }
+
+    public void createUser(CreateUserRequest request) {
+        PushNotificationStatus pushStatus = PushNotificationStatus.from(request.pushStatus());
+        AccountStatus accountStatus = AccountStatus.from(request.accountStatus());
+        User user = User.of(
+                request.oUserId(),
+                UserName.from(request.name()),
+                FcmToken.from(request.fcmToken()),
+                pushStatus,
+                request.authType(),
+                accountStatus
+        );
+        userRepository.save(user);
     }
 }
