9d5e6bdc21838355c280e485254204804ff0f2f8
Willy <willy_1020@naver.com>
2025-03-29 22:14:32 +0900

    [✅ chore] 유저 회원가입 성공 시 공통 응답값 처리

--- src/main/java/org/terning/user/common/success/UserSuccessCode.java
+++ src/main/java/org/terning/user/common/success/UserSuccessCode.java
@@ -10,7 +10,7 @@ import org.terning.global.success.SuccessCode;
 public enum UserSuccessCode implements SuccessCode {
 
     FCM_TOKEN_REISSUE_STATUS_PROVIDED(HttpStatus.OK, "FCM 토큰 재발급 여부를 성공적으로 확인했습니다."),
-
+    USER_CREATED(HttpStatus.CREATED, "유저가 성공적으로 생성되었습니다.")
     ;
 
     private final HttpStatus status;
