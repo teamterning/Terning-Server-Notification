a559bd3f09bfb6bf3eaeef7a74dae1e9d2d80516
권장순 <jsoonworld@gmail.com>
2025-04-06 08:17:04 +0900

    refactor(SendSchedulePolicy) : MessageWeeklyPolicy를 Schedules로 리팩터링하여 적용
    스케줄 리스트를 감싸는 객체를 일급 컬렉션인 Schedules로 변경하고,
    SendSchedulePolicy에서 기존 MessageWeeklyPolicy 대신 Schedules를 사용하도록 수정했습니다.

--- src/main/java/org/terning/message/domain/enums/SendSchedulePolicy.java
+++ src/main/java/org/terning/message/domain/enums/SendSchedulePolicy.java
@@ -1,6 +1,6 @@
 package org.terning.message.domain.enums;
 
-import org.terning.message.domain.vo.MessageWeeklyPolicy;
+import org.terning.message.domain.Schedules;
 import org.terning.message.domain.vo.Schedule;
 
 import java.time.DayOfWeek;
@@ -9,20 +9,20 @@ import java.time.LocalTime;
 import java.util.List;
 
 public enum SendSchedulePolicy {
-    INTERESTED_ANNOUNCEMENT(MessageWeeklyPolicy.of(List.of(
+    INTERESTED_ANNOUNCEMENT(Schedules.of(List.of(
             new Schedule(DayOfWeek.MONDAY, LocalTime.of(17, 0)))
     )),
-    INTERNSHIP_RECOMMENDATION(MessageWeeklyPolicy.of(List.of(
+    INTERNSHIP_RECOMMENDATION(Schedules.of(List.of(
             new Schedule(DayOfWeek.THURSDAY, LocalTime.of(13, 0)),
             new Schedule(DayOfWeek.SATURDAY, LocalTime.of(13, 0))
     ))),
-    TRENDING_INTERNSHIP(MessageWeeklyPolicy.of(List.of(
+    TRENDING_INTERNSHIP(Schedules.of(List.of(
             new Schedule(DayOfWeek.SUNDAY, LocalTime.of(21, 0)))
     ));
 
-    private final MessageWeeklyPolicy schedule;
+    private final Schedules schedule;
 
-    SendSchedulePolicy(MessageWeeklyPolicy schedule) {
+    SendSchedulePolicy(Schedules schedule) {
         this.schedule = schedule;
     }
 
