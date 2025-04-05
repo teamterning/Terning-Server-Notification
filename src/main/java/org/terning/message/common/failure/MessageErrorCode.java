4e67b4637d5a068b919f0043e0320550247d6870
권장순 <jsoonworld@gmail.com>
2025-04-06 08:14:32 +0900

    feat(MessageErrorCode) : 스케줄 리스트 비어 있음에 대한 예외 코드 추가
    스케줄 리스트가 비어 있을 경우 발생할 수 있는 런타임 예외를 방지하기 위해
    MessageErrorCode에 INVALID_EMPTY_SCHEDULE_LIST 항목을 추가했습니다.

--- src/main/java/org/terning/message/common/failure/MessageErrorCode.java
+++ src/main/java/org/terning/message/common/failure/MessageErrorCode.java
@@ -15,6 +15,8 @@ public enum MessageErrorCode implements ErrorCode {
     MISSING_PLACEHOLDER_VALUE(HttpStatus.BAD_REQUEST, "메시지 포맷팅에 필요한 값이 없습니다."),
 
     INVALID_SCHEDULE(HttpStatus.BAD_REQUEST, "유효하지 않은 스케줄입니다."),
+    INVALID_EMPTY_SCHEDULE_LIST(HttpStatus.BAD_REQUEST, "스케줄 리스트는 비어 있을 수 없습니다."),
+
 
     ;
 
