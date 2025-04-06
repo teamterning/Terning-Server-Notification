188c354c21af33335e635f2e1572f7497822edef
Willy <willy_1020@naver.com>
2025-04-06 10:19:32 +0900

    [✨ feat] 유저 상태 변경 관련 공통응답 포맷 추가

--- src/main/java/org/terning/user/common/success/UserSuccessCode.java
+++ src/main/java/org/terning/user/common/success/UserSuccessCode.java
@@ -11,7 +11,10 @@ public enum UserSuccessCode implements SuccessCode {
 
     FCM_TOKEN_REISSUE_STATUS_PROVIDED(HttpStatus.OK, "FCM 토큰 재발급 여부를 성공적으로 확인했습니다."),
     FCM_TOKEN_UPDATED(HttpStatus.CREATED, "FCM 토큰이 성공적으로 업데이트되었습니다."),
-    USER_CREATED(HttpStatus.CREATED, "유저가 성공적으로 생성되었습니다.")
+    USER_CREATED(HttpStatus.CREATED, "유저가 성공적으로 생성되었습니다."),
+    PUSH_STATUS_UPDATED(HttpStatus.OK, "유저 푸시알림 허용 여부 변경이 성공적으로 업데이트 되었습니다."),
+    USER_NAME_UPDATED(HttpStatus.OK, "유저이름이 성공적으로 업데이트 되었습니다."),
+    USER_DELETED(HttpStatus.OK, "유저가 성공적으로 삭제되었습니다.")
     ;
 
     private final HttpStatus status;
