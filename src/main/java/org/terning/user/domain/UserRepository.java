1bd4cf6ad637651cc05aaf5831e3a7d9f32ee93a
Willy <willy_1020@naver.com>
2025-04-06 10:18:12 +0900

    [✨ feat] oUserId를 통해 운영서버 <> 알림서버간 동기화를 진행할 수 있도록 메서드 추가

--- src/main/java/org/terning/user/domain/UserRepository.java
+++ src/main/java/org/terning/user/domain/UserRepository.java
@@ -1,7 +1,6 @@
 package org.terning.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 }
