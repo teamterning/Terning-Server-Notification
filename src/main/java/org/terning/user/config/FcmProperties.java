1165206d13a7ad15239e4431eee1a92ddad5cb2b
정윤 <sju08227@naver.com>
2025-04-07 23:16:09 +0900

    Update FcmProperties.java

--- src/main/java/org/terning/user/config/FcmProperties.java
+++ src/main/java/org/terning/user/config/FcmProperties.java
@@ -1,8 +1,9 @@
 package org.terning.user.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "firebase")
