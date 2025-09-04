6af5325fa64beb5509ca4e313b134ea355cb4e45
Willy <willy_1020@naver.com>
2025-09-04 14:45:19 +0900

    [FIX] 푸시 연동 테스트를 위한 알림 시간 변경

--- src/main/java/org/terning/fcm/application/FcmPushScheduler.java
+++ src/main/java/org/terning/fcm/application/FcmPushScheduler.java
@@ -39,7 +39,7 @@ public class FcmPushScheduler {
     }
 
     // 2. 매주 목/토 13:00 최근 공고 알림 생성 + 전송
-    @Scheduled(cron = "0 35 14 ? * THU,SAT", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 13 ? * THU,SAT", zone = "Asia/Seoul")
     public void sendRecentlyPostedInternshipRecommendation() {
         log.info("최근 공고 알림 생성을 시작합니다.");
         createAndSendNotifications("RECENTLY_POSTED_INTERNSHIP_RECOMMENDATION");
